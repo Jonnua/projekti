c92fd209585a7cdae0e0d00979b63c89ba9c9d53
narticerkini <narticerkini@gmail.com>
2024-02-03 23:49:27 +0100

    Update register.js

--- js/register.js
+++ js/register.js
@@ -1,50 +1,55 @@
-function validateFormRegister(){
-    var name = document.forms["myForm"]["name"]; 
-    var surname = document.forms["myForm"]["surname"];          
-    var email = document.forms["myForm"]["email"];    
-    var password= document.forms["myForm"]["password"];
+// Kjo është funksioni JavaScript për validimin e formës së regjistrimit.
 
-    if (name.value == "")                                  
-    { 
-        document.getElementById('errorname').innerHTML="Please enter a valid name!";  
-        return false; 
-    }else{
-        document.getElementById('errorname').innerHTML="";  
+function validateFormRegister() {
+    var name = document.forms["myForm"]["name"].value;
+    var surname = document.forms["myForm"]["surname"].value;
+    var email = document.forms["myForm"]["email"].value;
+    var password = document.forms["myForm"]["password"].value;
+    var isValid = true;
+
+    var nameRegex = /^[a-zA-Z\s]*$/;
+    if (name == "") {
+        document.getElementById('errorname').innerHTML = "Ju lutem jepni një emër të vlefshëm!";
+        isValid = false;
+    } else if (!nameRegex.test(name)) {
+        document.getElementById('errorname').innerHTML = "Ju lutem jepni një emër të vlefshëm!";
+        isValid = false;
+    } else {
+        document.getElementById('errorname').innerHTML = "";
     }
 
-    if (surname.value == "")                                  
-    { 
-        document.getElementById('errorsurname').innerHTML="Please enter a valid surname!";  
-        return false; 
-    }else{
-        document.getElementById('errorsurname').innerHTML="";  
+    var surnameRegex = /^[a-zA-Z\s]*$/;
+    if (surname == "") {
+        document.getElementById('errorsurname').innerHTML = "Ju lutem jepni një mbiemër të vlefshëm!";
+        isValid = false;
+    } else if (!surnameRegex.test(surname)) {
+        document.getElementById('errorsurname').innerHTML = "Ju lutem jepni një mbiemër të vlefshëm!";
+        isValid = false;
+    } else {
+        document.getElementById('errorsurname').innerHTML = "";
     }
 
-    if (email.value == "")                                   
-    { 
-        document.getElementById('erroremail').innerHTML="Please enter a valid email address!"; 
-        return false; 
-    }else{
-        document.getElementById('erroremail').innerHTML="";  
-    }
-   
-    if (email.value.indexOf("@", 0) < 0)                 
-    { 
-        document.getElementById('erroremail').innerHTML="Please enter a valid email address!"; 
-       
-        return false; 
-    } 
-
-    if (password.value == "")                                  
-    { 
-        document.getElementById('errorpassword').innerHTML="Please enter a valid password";  
-        return false; 
-    }else{
-        document.getElementById('errorpassword').innerHTML=""; 
-        alert("You are registered! You can try and login now!"); 
-        
+    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (email == "") {
+        document.getElementById('erroremail').innerHTML = "Ju lutem jepni një adresë email-i të vlefshme!";
+        isValid = false;
+    } else if (!emailRegex.test(email)) {
+        document.getElementById('erroremail').innerHTML = "Ju lutem jepni një adresë email-i të vlefshme!";
+        isValid = false;
+    } else {
+        document.getElementById('erroremail').innerHTML = "";
     }
 
+    var passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
+    if (password == "") {
+        document.getElementById('errorpassword').innerHTML = "Ju lutem jepni një fjalëkalim të vlefshëm";
+        isValid = false;
+    } else if (!passwordRegex.test(password)) {
+        document.getElementById('errorpassword').innerHTML = "Ju lutem jepni një fjalëkalim të vlefshëm (të paktën 8 karaktere, një shkronjë të madhe, një shkronjë të vogël dhe një numër).";
+        isValid = false;
+    } else {
+        document.getElementById('errorpassword').innerHTML = "";
+    }
 
-
+    return isValid;
 }
