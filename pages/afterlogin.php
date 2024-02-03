b610c4e086d6560abc36e38c183a58be726314bd
narticerkini <narticerkini@gmail.com>
2024-02-03 23:49:26 +0100

    Create afterlogin.php

---
+++ pages/afterlogin.php
@@ -0,0 +1,253 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../css/style.css">
+  <title>Home</title>
+</head>
+
+<body>
+<?php
+session_start();
+if (!isset($_SESSION['user_id']) || $_SESSION['user_type'] != 0) {
+    header("Location: login.php");
+    exit();
+}
+
+?>
+  <section id="header">
+    <div class="header container">
+      <div class="nav-bar">
+        <div class="brand">
+          <a href="index.php">
+            <h1><span>J</span>O- <span>N</span>A <span>T</span>RAVEL</h1>
+          </a>
+        </div>
+        <div class="nav-list">
+          <div class="hamburger">
+            <div class="bar"></div>
+          </div>
+          <ul>
+            <li><a href="#hero" data-after="Home">Home</a></li>
+            <li><a href="#services" data-after="Service">Transporti</a></li>
+            <li><a href="#projects" data-after="Service">Package</a></li>
+            <li><a href="#about" data-after="About">About</a></li>
+            <li><a href="#contact" data-after="Contact">Contact</a></li>
+            <li><a href="book.php" data-after="Book">Book</a></li>
+            <li><a href="../config/logout.php" data-after="Home">LogOut</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section id="hero">
+    <div class="hero container">
+      <section class="container-slider">
+        <div class="slider-wrapper">
+          <div class="slider">
+            <img id="slider-1" src="../img/home-slide-1.jpg" alt="">
+            <img src="../img/home-slide-2.jpg" alt="" id="slider-2">
+            <img src="../img/home-slide-3.jpg" alt="" id="slider-3">
+          </div>
+          <div class="slider-nav">
+            <a href="#slider-1"></a>
+            <a href="#slider-2"></a>
+            <a href="#slider-3"></a>
+          </div>
+
+
+        </div>
+
+
+
+        <div>
+          <h1>JO-NA <span></span></h1>
+          <h1>Travel <span></span></h1>
+        </div>
+      </section>
+    </div>
+  </section>
+  <section id="services">
+    <div class="services container">
+      <div class="service-top">
+        <h1 class="section-title">Transporti <span>i</span> JO-NA Travel</h1>
+
+      </div>
+      <div class="service-bottom">
+        <div class="service-item">
+          <div class="icon"><img src="../img/bus.png" /></div>
+          <h2>Autobus</h2>
+          <p>Faqja e agjensionit turistik ofron autobusë
+            të fundit të modelit, të pajisur me klimë,
+            internet dhe sisteme të avancuara të zënies së vendit. Informacioni i detajuar rreth itinerarit dhe
+            destinacioneve është lehtësisht i arritshëm. </p>
+        </div>
+        <div class="service-item">
+          <div class="icon"><img src="../img/taxi.png" /></div>
+          <h2>Taxi</h2>
+          <p>Taxi është një mjet transporti i përdorur për transport individual. Përfaqëson një zgjedhje të
+            shpejtë dhe të rehatshme për udhëtarët që kanë nevojë për shërbim të menjëhershëm. Taxi të zakonshëm janë të
+            pajisur me taksimetra për caktimin e kostos së udhëtimit dhe ofrojnë një mjet të sigurt dhe të përshtatshëm
+            për udhëtimet në qytet. Shoferët e taksive janë të trajnuar dhe të njohur me rrugët lokale, duke siguruar
+            një shërbim të besueshëm dhe të efikasëm për udhëtarët.</p>
+        </div>
+        <div class="service-item">
+          <div class="icon">
+            <img src="../img/cargo-ship.png" />
+          </div>
+          <h2>Anije</h2>
+          <p>
+            Anija është një mjet i madh dhe i fuqishëm i përdorur për udhëtime detare. Përveç strukturës së fortë dhe
+            sigurisë, ajo ofron shërbime të avancuara si sisteme navigimi, restorante, dhe komoditete të shumta për
+            udhëtarët. Kjo bën që udhëtimet mbi ujë të jenë eksperiencë relaksuese dhe përshtatëse për pushim dhe
+            aventurë.</p>
+        </div>
+        <div class="service-item">
+          <div class="icon"><img src="../img/airplane.png" /></div>
+          <h2>Aeroplan</h2>
+          <p>
+            Aeroplanet tona janë mjete moderne të dizajnuara për transport ajror, ofrojnë udhëtime të shpejta dhe të
+            rehatshme me kapacitet për shumë udhëtarë. Pajisur me teknologji të avancuar, ata sigurojnë një mjet efikas
+            dhe të sigurt për t'u lëvizur midis destinacioneve.</p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section id="projects">
+    <div class="projects container">
+      <div class="projects-header">
+        <h1 class="section-title">Pac<span>k</span>ages</h1>
+      </div>
+      <div class="all-projects">
+        <div class="project-item">
+          <div class="project-info">
+            <h1>Maldive</h1>
+            <h2></h2>
+            <p>Maldive, një paradisë ishujsh në Oqeanin Indian, ofron
+              plazhe të bardha, ujë të kaltër kristal dhe një ambient
+              të mrekullueshëm tropikal. Me korallat e ngjyrosura dhe faunën e pasur të detit, është një destinacion i
+              ëndrrave për zhytësit. Ishujt e Maldive janë të njohur për rërën e butë dhe ujërat e qeta, duke krijuar
+              një atmosferë idilike për pushuesit. Një eksperiencë e paharrueshme në një destinacion turistik të
+              mrekullueshëm.</p>
+          </div>
+          <div class="project-img">
+            <img src="../img/header-bg-1.png" alt="img">
+          </div>
+        </div>
+        <div class="project-item">
+          <div class="project-info">
+            <h1>New York</h1>
+            <h2></h2>
+            <p>New York, metropol dinamik me siluetën e paharrueshme,
+              Times Square, Central Park dhe jetë të pasur kulturore. Shoppingu në Fifth Avenue, gastronomia e larmishme
+              dhe një jetë e natës që nuk pushon. Ajo qytet që asnjëherë nuk ftohet dhe skajshmëria e dritave
+              që bënë "Manhattan skyline" një pamje magjepsëse në çdo kohë.</p>
+          </div>
+          <div class="project-img">
+            <img src="../img/header-bg-2.png" alt="img">
+          </div>
+        </div>
+        <div class="project-item">
+          <div class="project-info">
+            <h1>Bali</h1>
+            <h2></h2>
+            <p>
+              Bali, një oazë e bukur në Indonezi me plazhe të arta,
+              pemë kokosi dhe tempuj hinduistë. Zonat si Ubud ofrojnë një mjedis artistik dhe kulturore, ndërsa plazhet
+              si Kuta dhe Seminyak ofrojnë aventura dhe rrethim tropikal. Bali është një destinacion i përsosur për
+              zhytjen, meditimin, dhe për
+              të eksploruar kulturën unike dhe gjelbërimin e saj natyror.</p>
+          </div>
+          <div class="project-img">
+            <img src="../img/header-bg-3.png" alt="img">
+          </div>
+        </div>
+        <div class="project-item">
+          <div class="project-info">
+            <h1>Dubai</h1>
+            <h2></h2>
+            <p>
+              Dubai, metropol modern me skyscraper-a të mahnitshëm,
+              shoppig në "Dubai Mall" dhe aventura në shiun e shkretëtirës. "Burj Khalifa" dhe plazhet e "Palm Jumeirah"
+              bëjnë që qyteti të jetë një destinacion shpërthyesor i pasurisë dhe stilin ejetës.
+            </p>
+          </div>
+          <div class="project-img">
+            <img src="../img/home-slide-1.jpg" alt="img">
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section id="about">
+    <div class="about container">
+      <div class="col-left">
+        <div class="about-img">
+          <img src="../img/jntravel.jpg" alt="img">
+        </div>
+      </div>
+      <div class="col-right">
+        <h1 class="section-title">About <span>Us</span></h1>
+        <h2>JO-NA Travel</h2>
+        <p>
+          Jo-Na Travel, udhëheqës në shërbimet turistike, ofron eksperienca të paharrueshme udhëtimi. Me destinacione të
+          çuditshme dhe paketa ekskluzive, ne krijojmë aventura të personalizuara për udhëtarët tanë. Shërbimi ynë i
+          përkushtuar dhe përvoja e pasur turistike bëjnë Jo-Na Travel zgjedhjen perfekte për udhëtimet tuaja.</p>
+      </div>
+    </div>
+  </section>
+  <section id="contact">
+    <div class="contact container">
+      <div>
+        <h1 class="section-title">Contact <span>info</span></h1>
+      </div>
+      <div class="contact-items">
+        <div class="contact-item">
+          <div class="icon"><img src="https://img.icons8.com/bubbles/100/000000/phone.png" /></div>
+          <div class="contact-info">
+            <h1>Phone</h1>
+            <h2>+3834444444</h2>
+            <h2>+3834444444</h2>
+            <h2>+3834444444</h2>
+          </div>
+        </div>
+        <div class="contact-item">
+          <div class="icon"><img src="https://img.icons8.com/bubbles/100/000000/new-post.png" /></div>
+          <div class="contact-info">
+            <h1>Email</h1>
+            <h2>info@jo-natravel.com</h2>
+            <h2>contact@jo-natravel.com</h2>
+          </div>
+        </div>
+        <div class="contact-item">
+          <div class="icon"><img src="https://img.icons8.com/bubbles/100/000000/map-marker.png" /></div>
+          <div class="contact-info">
+            <h1>Address</h1>
+            <h2>Prishtina, Kosove</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section id="footer">
+    <div class="footer container">
+      <div class="brand">
+        <h1><span>J</span>O <span>-N</span>A</h1>
+      </div>
+      <div class="social-icon">
+        <div class="social-item">
+          <a href="#"><img src="https://img.icons8.com/bubbles/100/000000/facebook-new.png" /></a>
+        </div>
+        <div class="social-item">
+          <a href="#"><img src="https://img.icons8.com/bubbles/100/000000/instagram-new.png" /></a>
+        </div>
+      </div>
+      <p>Copyright © 2023 JO-NA. All rights reserved</p>
+    </div>
+  </section>
+  <script src="./js/main.js"></script>
+</body>
+
+</html>
