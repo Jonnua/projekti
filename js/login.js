ec70e7fc807aedf2673686edf9a9ee0574562c0b
Jonnua <jonarrahmani4@gmail.com>
2024-02-03 23:45:48 +0100

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,30 +1,31 @@
-function validateFormLogin(){ 
-               
-    var email = document.forms["myForm"]["email"].value;    
+// Kjo është funksioni që validon formën e hyrjes në panelin e kontrollit të përdoruesve.
+function validateLoginForm() {
+    // Merr vlerën e email-it dhe fjalëkalimit nga formulari
+    var email = document.forms["loginForm"]["email"].value;
+    var password = document.forms["loginForm"]["password"].value;
+    var isValid = true;
 
-    var password = document.forms["myForm"]["password"].value;   
+    // Kontrollon nëse email-i është bosh
+    if (email == "") {
+        document.getElementById('erroremail').innerHTML = "Ju lutemi vendosni adresën tuaj të email-it";
+        isValid = false;
+    } 
+    // Kontrollon nëse email-i ka karakterin "@"
+    else if (email.indexOf("@", 0) < 0) {
+        document.getElementById('erroremail').innerHTML = "Ju lutemi vendosni një adresë të vlefshme email-i";
+        isValid = false;
+    } else {
+        document.getElementById('erroremail').innerHTML = "";
+    }
 
-    var emailRegex=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    // Kontrollon nëse fjalëkalimi është bosh
+    if (password == "") {
+        document.getElementById('errorpassword').innerHTML = "Ju lutemi vendosni fjalëkalimin tuaj";
+        isValid = false;
+    } else {
+        document.getElementById('errorpassword').innerHTML = "";
+    }
 
-
-    var passwordRegex=/^[A-Z]{1}[a-zA-Z0-9!@#\$%\^\&*\)\(+=._-]{6}$/;
-
-
-   
-   if(!(emailRegex.test(email))) {
-    document.getElementById('erroremail').innerHTML="Please enter a valid email address"; 
-   return false;
-   }
-   else{
-    document.getElementById('erroremail').innerHTML=""; 
-   }
-   if(!(passwordRegex.test(password))){
-    document.getElementById('errorpassword').innerHTML="Please enter a valid password";
-    return false;
-   }
-   else{
-    document.getElementById('errorpassword').innerHTML="";
-    alert("You've logged in successfully!");  
-   }
-  
-}  
+    // Kthe true nëse të gjitha validimet janë kaluar, në të kundërt kthe false
+    return isValid;
+}
